d6cb224cc0cd50f32324a0a958d7a88b7e4652f0
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2023-08-11 21:45:38 -0700

    impl amin and main shop routers

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 const express = require("express");
 const path = require("path");
+const bodyParser = require("body-parser");
+const adminRouter = require("./routes/admin");
+const shopRouter = require("./routes/shop")
 
 const port = 3030;
-
 const app = express();
 
 app.set("views,views");
 
+// static مشخص کردن مسیر فایل های 
 app.use(express.static(path.join(__dirname,"public")))
+// داده های ارسالی ریسپانس ها رو به جیسون تبدیل میکنه
+app.use(bodyParser.urlencoded({extended: false}))
+// نوشته بشه admin قبل از ریکوئست های پنل ادمین حتما باید 
+app.use("/admin",adminRouter);
+app.use(shopRouter)
 
-app.get("/",(req,res) => {
-    res.sendFile(path.join(__dirname,"views","index.html"));
-});
 
 app.listen(port,()=>{
     console.log("Listeninig on port :",port)
