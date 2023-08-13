06fc6a9d68b38c429c49103ecf686be72da48137
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2023-08-12 23:15:52 -0700

    impl add producte to database function

--- controllers/admin_controller.js
+++ controllers/admin_controller.js
@@ -1,8 +1,29 @@
-const product = require("../models/product")
+const Product = require("../models/product")
 
 
 function getAddProducte(req,res){
     res.render("admin/add-product")
 }
 
-module.exports = {getAddProducte};
+function addPostProduct(req,res){
+
+    const title = req.body.title;
+    const content = req.body.content;
+    const price = req.body.price;
+    const imageUrl = req.body.imageUrl;
+
+    const product = new Product({
+        title : title,
+        content : content,
+        price : price,
+        imageurl : imageUrl,
+    })
+
+    product.save().then(result => {
+        console.log("product added....");
+        res.redirect('/')
+    })
+
+}
+
+module.exports = {getAddProducte,addPostProduct};
--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const adminController = require("../controllers/admin_controller")
 
-router.get("/add-product",adminController.getAddProducte)
+router.get("/add-product",adminController.getAddProducte);
+router.post("/add-product",adminController.addPostProduct);
 
 module.exports = router
