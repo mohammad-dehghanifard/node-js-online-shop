5f84f5af06a0de3fa19230b54cd88e37d85f1ae0
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2023-08-18 23:43:30 -0700

    impl user model

---
+++ models/user.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+    {
+        name : {
+            type : String,
+            required:  true,
+            unique : true,
+            minlength : 3,
+            maxlength : 15,
+        },
+        email : {
+            type : String,
+            required:  true,
+            unique : true,
+            minlength : 8,
+            maxlength : 35,
+        },
+        cart : {
+            items : [
+                {
+                    productId : {
+                        type : Schema.Types.ObjectId,
+                        ref : "Product",
+                        required : true,
+                    },
+                    quantity : {
+                        type : Number,
+                        required : true
+                    }
+                }
+            ]
+        },
+    }
+)
+
+module.exports = mongoose.model("User",userSchema)
