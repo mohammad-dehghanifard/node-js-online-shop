2306b1eb714cfb5b6bf1e9466121f89bac4b3ae4
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2023-09-26 22:38:28 -0700

    save login state in sessions

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -5,13 +5,13 @@ exports.renderLoginPage = (req,res) => {
     res.render("auth/login",{
         path: "/login",
         pageTitle : "ورود به حساب کاربری",
-        isAuthenticated : false,
+        isAuthenticated : isLogged["loggedIn"],
     })
     
 }
 
 exports.postLogin = (req,res) => {
     // ذخیره وضعیت لاگین کاربر در کوکی ها
-    res.setHeader('Set-Cookie',"loggedIn=true");
+    req.session.loggedIn = true;
     res.redirect("/");
 }
