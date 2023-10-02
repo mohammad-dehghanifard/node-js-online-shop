b546333155be40efea813f6ad4eeeffd7661636f
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2023-10-02 01:26:16 -0700

    impl isAuth Middlewere

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const adminController = require("../controllers/admin_controller")
+const adminController = require("../controllers/admin_controller");
+const isAuth = require("../middleware/is-auth");
 
-router.get("/add-product",adminController.getAddProducte);
+router.get("/add-product",isAuth,adminController.getAddProducte);
 router.post("/add-product",adminController.addPostProduct);
-router.get("/products",adminController.getAllProducts);
+router.get("/products",isAuth,adminController.getAllProducts);
 router.get("/edit-product/:productId",adminController.getEditProduct)
 router.post("/edit-product",adminController.postEditProduct)
 router.post("/delete-product",adminController.deleteProduct)
